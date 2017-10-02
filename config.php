9250598b64ff2baa926c606ebb68e2223380eb44
mifsudm <mifsudm@unimelb.edu.au>
2017-10-03 08:18:15 +1100

    Minor Code Updates - 252s-dev

--- config.php
+++ config.php
@@ -12,11 +12,7 @@ $routes = $config['site.routes'];
 $params = array('role' => 'admin');
 $routes->add('Skill Admin Settings', new \Tk\Routing\Route('/skill/adminSettings.html', 'Skill\Controller\SystemSettings::doDefault', $params));
 
-$params = array('role' => array('admin', 'client'));
-//$routes->add('Skill Institution Settings', new \Tk\Routing\Route('/skill/institutionSettings.html', 'Skill\Controller\InstitutionSettings::doDefault', $params));
-
 $params = array('role' => array('client', 'staff'));
 $routes->add('Skill Profile Settings', new \Tk\Routing\Route('/skill/profileSettings.html', 'Skill\Controller\ProfileSettings::doDefault', $params));
 
-//$routes->add('Skill Course Settings', new \Tk\Routing\Route('/skill/courseSettings.html', 'Skill\Controller\CourseSettings::doDefault', $params));
 
