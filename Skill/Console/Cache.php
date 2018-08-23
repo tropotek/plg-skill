fed643f59941556b4109746ff8f7bb0f4d76d9c7
godar <info@tropotek.com>
2018-08-24 05:42:45 +1000

    ~Auto: Commit

--- Skill/Console/Cache.php
+++ Skill/Console/Cache.php
@@ -36,44 +36,12 @@ class Cache extends \Tk\Console\Console
         // required vars
         $config = \App\Config::getInstance();
 
-        $subjects = \App\Db\SubjectMap::create()->findFiltered(array('active' => true), \Tk\Db\Tool::create('id DESC'));
-        if ($subjects->count()) {
-            $this->write('');
-            $this->write(' - Updating gradable skill results cache.');
+        if (\App\Config::getInstance()->getEventDispatcher()) {
+            $e = new \Tk\Event\Event();
+            $e->set('console', $this);
+            \App\Config::getInstance()->getEventDispatcher()->dispatch(\App\AppEvents::CONSOLE_CRON, $e);
         }
-        foreach ($subjects as $subject) {
-            $collections = \Skill\Db\CollectionMap::create()->findFiltered(array(
-                    'gradable' => true,
-                    'requirePlacement' => true,
-                    'subjectId' => $subject->getId())
-            );
-            $this->writeComment('', Output::VERBOSITY_VERY_VERBOSE);
-            foreach ($collections as $collection) {
-                $students = \App\Db\UserMap::create()->findFiltered(array('subjectId' => $subject->getId(), 'role' => \App\Db\UserGroup::ROLE_STUDENT));
-                $this->writeComment($subject->name . ' - ' . $collection->name, Output::VERBOSITY_VERY_VERBOSE);
-                $this->writeComment('  - Collection ID: ' . $collection->getId(), Output::VERBOSITY_VERY_VERBOSE);
-                $this->writeComment('  - Subject ID:    ' . $subject->getId(), Output::VERBOSITY_VERY_VERBOSE);
-                $this->writeComment('  - Students:      ' . $students->count(), Output::VERBOSITY_VERY_VERBOSE);
-
-                $res = \Skill\Util\Calculator::findSubjectAverageGrades($collection, $subject, true);  // re-cache results
-                if (!$res || !$res->count){
-                    $this->writeComment('  - Entry Count:   0', Output::VERBOSITY_VERY_VERBOSE);
-                    continue;
-                }
-
-                $this->writeComment('  - Entry Count:   ' . $res->subjectEntryCount, Output::VERBOSITY_VERY_VERBOSE);
-                $this->writeComment('  - Min:           ' . $res->min, Output::VERBOSITY_VERY_VERBOSE);
-                $this->writeComment('  - Median:        ' . $res->median, Output::VERBOSITY_VERY_VERBOSE);
-                $this->writeComment('  - Max:           ' . $res->max, Output::VERBOSITY_VERY_VERBOSE);
-                $this->writeComment('  - Avg:           ' . $res->avg, Output::VERBOSITY_VERY_VERBOSE);
-            }
-        }
-
-
-        $this->writeComment('TODO: Not implemented Yet');
 
     }
-
-
 
 }
