80da946f41eab9abf8ff8488ad67da4ad182df3d
Tropotek <info@tropotek.com>
2018-11-09 09:50:21 +1100

    ~Auto: Commit

--- Skill/Db/ReportingMap.php
+++ Skill/Db/ReportingMap.php
@@ -45,88 +45,6 @@ class ReportingMap
 
     /**
      *
-     *
-     * @param $collectionId
-     * @param $subjectId
-     * @param int $userId
-     * @param bool $valueOnly
-     * @param string $entryStatus
-     * @param string $placementStatus
-     * @return array|int
-     * @throws \Tk\Db\Exception
-     */
-    public function findStudentResult($collectionId, $subjectId, $userId = 0, $valueOnly = false, $entryStatus = 'approved', $placementStatus = 'completed')
-    {
-        $usql = '';
-        if ($userId) {
-            $usql = ' AND a.user_id = ? ';
-        }
-        
-        $sql = <<<SQL
-SELECT a.collection_id, a.user_id, a.subject_id, SUM(a.weighted_avg) / a.scale AS 'subject_result'
-  FROM
-    (SELECT a.collection_id, a.user_id, a.subject_id, a.domain_id, a.label, c.scale, a.weight,
-      SUM(a.average) / b.count AS 'avg', (SUM(a.average) / b.count) * a.weight AS 'weighted_avg'
-    FROM
-      (
-        SELECT a.collection_id, a.subject_id, a.user_id, c.id AS 'item_id', a.id AS 'entry_id', d.id AS 'domain_id',
-          d.label, c.question, ROUND(AVG(b.value), 2) AS 'average', d.order_by, d.weight
-        FROM skill_entry a LEFT JOIN placement p ON (a.placement_id = p.id), skill_value b, skill_item c, skill_domain d
-        WHERE
-          a.del = 0 AND c.del = 0 AND d.del = 0 AND
-              a.id = b.entry_id AND b.value > 0 AND
-              a.status = '$entryStatus' AND
-              (p.status IS NULL OR p.status = '$placementStatus') AND 
-              b.item_id = c.id AND
-              c.domain_id = d.id
-        GROUP BY a.collection_id, a.subject_id, a.user_id, b.item_id
-        ORDER BY d.order_by, c.order_by
-      ) a,
-      (
-        SELECT a.domain_id, COUNT(a.id) AS 'count'
-        FROM skill_item a
-        GROUP BY a.domain_id
-      ) b,
-      (
-        SELECT a.collection_id, COUNT(a.id) - 1 AS 'scale'
-        FROM skill_scale a
-        GROUP BY a.collection_id
-      ) c
-    WHERE
-      a.domain_id = b.domain_id AND
-          c.collection_id = a.collection_id
-
-    GROUP BY a.collection_id, a.subject_id, a.user_id, a.domain_id
-    ORDER BY a.order_by
-    ) a
-  WHERE
-    a.collection_id = ? AND a.subject_id = ? $usql
-  GROUP BY a.collection_id, a.subject_id, a.user_id
-SQL;
-        
-        $stm = $this->getDb()->prepare($sql);
-        $stm->bindParam(1, $collectionId);
-        $stm->bindParam(2, $subjectId);
-        if ($usql)
-            $stm->bindParam(3, $userId);
-        
-        $stm->execute();
-        $arr = $stm->fetchAll();
-        if ($valueOnly) {
-            if (!count($arr) && $userId) return 0;
-            $arr1 = array();
-            foreach ($arr as $obj) {
-                if ($userId) return $obj->subject_result;
-                $arr1[$obj->user_id] = $obj->subject_result;
-            }
-            $arr = $arr1;
-        }
-        return $arr;
-    }
-
-
-    /**
-     *
      * @param $collectionId
      * @param $subjectId
      * @param bool $valueOnly
@@ -192,6 +110,162 @@ SQL;
         return $arr;
     }
 
+
+    /**
+     *
+     *
+     * @param $collectionId
+     * @param $subjectId
+     * @param int $userId
+     * @param bool $valueOnly
+     * @param string $entryStatus
+     * @param string $placementStatus
+     * @return array|int
+     * @throws \Tk\Db\Exception
+     */
+    public function findStudentResult($collectionId, $subjectId, $userId = 0, $valueOnly = false, $entryStatus = 'approved', $placementStatus = 'completed')
+    {
+        $usql = '';
+        if ($userId) {
+            $usql = ' AND a.user_id = ? ';
+        }
+
+        $sql = <<<SQL
+SELECT a.collection_id, a.user_id, a.subject_id, SUM(a.weighted_avg) / a.scale AS 'subject_result'
+  FROM
+    (SELECT a.collection_id, a.user_id, a.subject_id, a.domain_id, a.label, c.scale, a.weight,
+      SUM(a.average) / b.count AS 'avg', (SUM(a.average) / b.count) * a.weight AS 'weighted_avg'
+    FROM
+      (
+        SELECT a.collection_id, a.subject_id, a.user_id, c.id AS 'item_id', a.id AS 'entry_id', d.id AS 'domain_id',
+          d.label, c.question, ROUND(AVG(b.value), 2) AS 'average', d.order_by, d.weight
+        FROM skill_entry a LEFT JOIN placement p ON (a.placement_id = p.id), skill_value b, skill_item c, skill_domain d
+        WHERE
+          a.del = 0 AND c.del = 0 AND d.del = 0 AND
+              a.id = b.entry_id AND b.value > 0 AND
+              a.status = '$entryStatus' AND
+              (p.status IS NULL OR p.status = '$placementStatus') AND 
+              b.item_id = c.id AND
+              c.domain_id = d.id
+        GROUP BY a.collection_id, a.subject_id, a.user_id, b.item_id
+        ORDER BY d.order_by, c.order_by
+      ) a,
+      (
+        SELECT a.domain_id, COUNT(a.id) AS 'count'
+        FROM skill_item a
+        GROUP BY a.domain_id
+      ) b,
+      (
+        SELECT a.collection_id, COUNT(a.id) - 1 AS 'scale'
+        FROM skill_scale a
+        GROUP BY a.collection_id
+      ) c
+    WHERE
+      a.domain_id = b.domain_id AND
+          c.collection_id = a.collection_id
+
+    GROUP BY a.collection_id, a.subject_id, a.user_id, a.domain_id
+    ORDER BY a.order_by
+    ) a
+  WHERE
+    a.collection_id = ? AND a.subject_id = ? $usql
+  GROUP BY a.collection_id, a.subject_id, a.user_id
+SQL;
+
+        $stm = $this->getDb()->prepare($sql);
+        $stm->bindParam(1, $collectionId);
+        $stm->bindParam(2, $subjectId);
+        if ($usql)
+            $stm->bindParam(3, $userId);
+
+        $stm->execute();
+        $arr = $stm->fetchAll();
+        if ($valueOnly) {
+            if (!count($arr) && $userId) return 0;
+            $arr1 = array();
+            foreach ($arr as $obj) {
+                if ($userId) return $obj->subject_result;
+                $arr1[$obj->user_id] = $obj->subject_result;
+            }
+            $arr = $arr1;
+        }
+        return $arr;
+    }
+
+
+
+    public function findStudentResultNew($collectionId, $subjectId, $userId = 0, $valueOnly = false, $entryStatus = 'approved', $placementStatus = 'completed')
+    {
+        $usql = '';
+        if ($userId) {
+            $usql = ' AND a.user_id = ? ';
+        }
+
+        $sql = <<<SQL
+SELECT a.collection_id, a.user_id, a.subject_id, SUM(a.weighted_avg) / a.scale_count AS 'subject_result'
+  FROM
+    (SELECT a.collection_id, a.user_id, a.subject_id, a.domain_id, a.label, a.weight, a.domain_count, a.scale_count,
+      SUM(a.average) / a.domain_count AS 'avg',
+      (SUM(a.average) / a.domain_count) * a.weight AS 'weighted_avg'
+    FROM
+      (
+        SELECT a.collection_id, a.subject_id, a.user_id, c.id AS 'item_id', a.id AS 'entry_id', d.id AS 'domain_id', d.order_by, d.weight,
+          d.label, c.question, ROUND(AVG(b.value), 2) AS 'average', e.domain_count, f.scale_count
+
+        FROM skill_entry a LEFT JOIN placement p ON (a.placement_id = p.id),
+            skill_value b, skill_item c, skill_domain d,
+          (
+            SELECT a.id, COUNT(a.id) AS 'domain_count'
+            FROM skill_domain a
+            WHERE a.collection_id = $collectionId
+            GROUP BY a.id
+          ) e,
+          (
+            SELECT a.collection_id, COUNT(a.id) - 1 AS 'scale_count'
+            FROM skill_scale a
+            WHERE a.collection_id = $collectionId
+            GROUP BY a.collection_id
+          ) f
+        WHERE
+          a.del = 0 AND c.del = 0 AND d.del = 0 AND
+              a.id = b.entry_id AND b.value > 0 AND
+              a.status = '$entryStatus' AND
+              (p.status IS NULL OR p.status = '$placementStatus') AND
+              b.item_id = c.id AND
+              c.domain_id = d.id
+        GROUP BY a.collection_id, a.subject_id, a.user_id, b.item_id
+        ORDER BY d.order_by, c.order_by
+      ) a
+    WHERE 1
+
+    GROUP BY a.collection_id, a.subject_id, a.user_id, a.domain_id
+    ORDER BY a.order_by
+    ) a
+  WHERE
+    a.collection_id = ? AND a.subject_id = ? $usql
+  GROUP BY a.collection_id, a.subject_id, a.user_id
+SQL;
+
+        $stm = $this->getDb()->prepare($sql);
+        $stm->bindParam(1, $collectionId);
+        $stm->bindParam(2, $subjectId);
+        if ($usql)
+            $stm->bindParam(3, $userId);
+
+        $stm->execute();
+        $arr = $stm->fetchAll();
+        if ($valueOnly) {
+            if (!count($arr) && $userId) return 0;
+            $arr1 = array();
+            foreach ($arr as $obj) {
+                if ($userId) return $obj->subject_result;
+                $arr1[$obj->user_id] = $obj->subject_result;
+            }
+            $arr = $arr1;
+        }
+        return $arr;
+    }
+
     /**
      * Find the student results by brake-down of domains
      * Find the average and total results for the student
@@ -228,19 +302,25 @@ SQL;
          * See: https://stackoverflow.com/questions/17964078/mysql-query-to-dynamically-convert-rows-to-columns-on-the-basis-of-two-columns
          */
         $sql = <<<SQL
-SELECT a.collection_id, a.user_id, a.domain_id, a.label, a.label_name, b.weight,
-  c.max_grade, ROUND(AVG(a.avg), 2) as 'avg',
-  (ROUND(AVG(a.avg), 2)*(c.max_grade/d.scale)) as 'grade', a.name, a.uid, a.entry_count
+SELECT a.collection_id, a.user_id, a.domain_id, a.label, a.label_name, b.weight, a.name, a.uid, a.entry_count, a.domain_count, c.max_grade,
+  ROUND(AVG(a.avg_zero), 2) as 'avg_zero',
+  (ROUND(AVG(a.avg_zero), 2)*(c.max_grade/d.scale)) as 'grade_zero',
+  
+  ROUND(AVG(a.avg), 2) as 'avg',
+  (ROUND(AVG(a.avg), 2)*(c.max_grade/d.scale)) as 'grade'
 FROM
   (
-    SELECT a.collection_id, a.subject_id, a.user_id, d.domain_id, c.item_id, d.question,
-      IFNULL(ROUND(AVG(NULLIF(c.value, 0)), 2), 0) AS 'avg', d.order_by, a.label, a.label_name, a.name, a.uid, COUNT(a.entry_id) as 'entry_count'
+    SELECT a.collection_id, a.subject_id, a.user_id, d.domain_id, a.weight, c.item_id, d.question, a.domain_count,
+           IFNULL(ROUND(AVG(NULLIF(c.value, 0)), 2), 0) AS 'avg',   -- Standard Avg 
+           IFNULL(ROUND(AVG(c.value), 2), 0) AS 'avg_zero',         -- Avg with zero values
+           d.order_by, a.label, a.label_name, a.name, a.uid, COUNT(a.entry_id) as 'entry_count'
     FROM
       (
-        SELECT a.collection_id, a.subject_id, a.user_id, a.entry_id, b.domain_id, c.id as 'item_id', c.name as 'label_name', c.label, b.question, b.order_by, a.name, a.uid
+        SELECT a.collection_id, a.subject_id, a.user_id, a.entry_id, b.domain_id, c.id as 'item_id', c.name as 'label_name', 
+               c.label, b.question, b.order_by, a.name, a.uid, c.weight, a.domain_count
         FROM
           (
-            SELECT a.collection_id, a.subject_id, a.user_id, b.id as 'entry_id', a.name, a.uid
+            SELECT a.collection_id, a.subject_id, a.user_id, b.id as 'entry_id', a.name, a.uid, e.domain_count
             FROM
               (
                 SELECT $collectionId as 'collection_id', b.subject_id, a.id as 'user_id', a.name, a.uid
@@ -251,6 +331,11 @@ FROM
                 GROUP BY a.id, b.subject_id
                 ORDER BY b.subject_id
               ) a,
+              (
+                SELECT a.collection_id, COUNT(a.id)-1 as 'domain_count'
+                FROM skill_domain a
+                WHERE a.collection_id = $collectionId
+              ) e,
               skill_entry b LEFT JOIN placement p ON (b.placement_id = p.id)
             WHERE
               b.del = 0 AND
@@ -320,27 +405,33 @@ Student Number	Name	PD	SB	CS	AW	BIOS	PD Grade	SB Grade	CS Grade	AW Grade	BIOS Gr
         $totalGrade = 0;
         foreach ($stm as $i => $row) {
             if (!array_key_exists($row->user_id, $arr)) {
-                $arr[$row->user_id] = array('userId' => $row->user_id, 'uid' => $row->uid, 'name' => $row->name, 'maxGrade' => $row->max_grade);
+                $arr[$row->user_id] = array(
+                    'userId' => $row->user_id,
+                    'uid' => $row->uid,
+                    'name' => $row->name,
+                    'maxGrade' => $row->max_grade,
+                    'domain_count' => $row->domain_count,
+                    'total' => 0,
+                    'total_grade' => 0,
+                    'total_zero' => 0,
+                    'total_grade_zero' => 0
+                );
             }
             $arr[$row->user_id][$row->label] = $row->avg;
             $arr[$row->user_id][$row->label.'_grade'] = $row->grade;
             $arr[$row->user_id][$row->label.'_weight'] = $row->weight;
-            if (!isset($arr[$row->user_id]['domain_count'])) {
-                $arr[$row->user_id]['domain_count'] = 0;
-                $arr[$row->user_id]['total'] = 0;
-                $arr[$row->user_id]['total_grade'] = 0;
-            }
-            $arr[$row->user_id]['domain_count']++;
             $arr[$row->user_id]['total'] += $row->avg;
             $arr[$row->user_id]['total_grade'] += $row->grade;
+            $arr[$row->user_id]['total_zero'] += $row->avg_zero;
+            $arr[$row->user_id]['total_grade_zero'] += $row->grade_zero;
         }
         // Not Correct (Try to fix this)
-//        foreach ($arr as $uid => $row) {
-//            if ($row['domain_count']) {
-//                $row['total'] = round($row['total'] / $row['domain_count'], 2);
-//                $row['total_grade'] = round($row['total_grade'] / $row['domain_count'], 2);
-//            }
-//        }
+        foreach ($arr as $uid => $row) {
+            $arr[$uid]['total'] = round($row['total'] / $row['domain_count'], 2);
+            $arr[$uid]['total_grade'] = round($row['total_grade'] / $row['domain_count'], 2);
+            $arr[$uid]['total_zero'] = round($row['total_zero'] / $row['domain_count'], 2);
+            $arr[$uid]['total_grade_zero'] = round($row['total_grade_zero'] / $row['domain_count'], 2);
+        }
 
         return $arr;
     }
--- todo.md
+++ todo.md
@@ -1,6 +1,24 @@
 #TODO
 
 
+HOW TO CALCULATE GRADES (GOALS)
+
+  1. For each individual entry we calculate the domain average excluding the zero values.
+     So when viewing a single Entries result we have the average caclulated without the zero values.
+     (This should be cached for fast data access)
+  2. When calculating a students overall grade. 
+    1. We compile the domain averages for each of the entries that exclude the zero values. (as above)
+       This involves adding all the item values for each domain and dividing the each result by the number of entries
+    2. We then add all these domain results for each entry and divide the result by the number of domains. Get the average domain value.
+    3. For the final grade result we need to calculate the weighted average of each domain also. This is calculated by multiplying
+       each average domain value by the doamin.weight value.  
+    4. Finally add all the domain values and divide by the number of domains to get the average grade. 
+    5. Divide this value by the scale and multiply by 100 to get the percentage grade
+  
+
+
+
+
 
   - The entire grade results system of caclculation is not working efficiently, we need to implement
     more caching and more optimised speedy queries to make it fully functional.
