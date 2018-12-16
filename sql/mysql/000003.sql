fda0a7dedd2f00ac766b6b85cbdd96fa37c2c2ed
mifsudm <mifsudm@unimelb.edu.au>
2018-12-17 10:35:04 +1100

    ~Auto: Commit

--- sql/mysql/000003.sql
+++ sql/mysql/000003.sql
@@ -3,36 +3,6 @@
 -- ----------------------------------
 
 
-
-#
-# -- -----------------------------------------------------
-# -- Use this table to store the average value of the domain
-# --
-# --
-# -- -----------------------------------------------------
-# CREATE TABLE IF NOT EXISTS skill_entry_grade (
-#   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-#
-#   entry_id INT UNSIGNED NOT NULL DEFAULT 0,
-#   user_id INT UNSIGNED NOT NULL DEFAULT 0,
-#   domain_id INT UNSIGNED NOT NULL DEFAULT 0,
-#   collection_id INT UNSIGNED NOT NULL DEFAULT 0,
-#   entry_status VARCHAR(64) NOT NULL DEFAULT '',
-#   placement_status VARCHAR(64) NOT NULL DEFAULT '',
-#
-#   domain_count INT UNSIGNED NOT NULL DEFAULT 0,
-#   scale_count INT UNSIGNED NOT NULL DEFAULT 0,
-#   weight FLOAT NOT NULL DEFAULT 1.0,                -- The domain weight to be applied to the average
-#
-#   avg FLOAT NOT NULL DEFAULT 0.0,
-#   zero_avg FLOAT NOT NULL DEFAULT 0.0,
-#
-#   KEY (entry_id),
-#   KEY (user_id),
-#   KEY (domain_id),
-#   KEY (collection_id)
-# ) ENGINE=InnoDB;
-
 UPDATE skill_category SET uid = '0' WHERE uid = '' OR uid IS NULL;
 alter table skill_category modify uid INT UNSIGNED default 0 not null;
 UPDATE skill_collection SET uid = '0' WHERE uid = '' OR uid IS NULL;
