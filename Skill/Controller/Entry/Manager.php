8bf2249878901ffaad25aae442e2cb7235eb92d0
mifsudm <mifsudm@unimelb.edu.au>
2019-02-14 12:44:46 +1100

    ~Auto: Commit

--- Skill/Controller/Entry/Manager.php
+++ Skill/Controller/Entry/Manager.php
@@ -90,18 +90,18 @@ class Manager extends AdminManagerIface
         $this->table->appendCell(new \Tk\Table\Cell\Date('created'));
 
         // Filters
-        $this->table->addFilter(new Field\Input('keywords'))->setAttr('placeholder', 'Keywords');
+        $this->table->appendFilter(new Field\Input('keywords'))->setAttr('placeholder', 'Keywords');
 
         $list = \App\Db\CompanyMap::create()->findFiltered(array(
             'profileId' => $this->getProfileId(),
             'status' => \App\Db\Placement::STATUS_APPROVED
         ), \Tk\Db\Tool::create('name'));
-        $this->table->addFilter(new Field\Select('companyId', $list))->prependOption('-- Company --', '');
+        $this->table->appendFilter(new Field\Select('companyId', $list))->prependOption('-- Company --', '');
 
         // Actions
-        $this->table->addAction(\Tk\Table\Action\ColumnSelect::create()->setDisabled(array('id', 'title')));
-        $this->table->addAction(\Tk\Table\Action\Csv::create());
-        $this->table->addAction(\Tk\Table\Action\Delete::create());
+        $this->table->appendAction(\Tk\Table\Action\ColumnSelect::create()->setDisabled(array('id', 'title')));
+        $this->table->appendAction(\Tk\Table\Action\Csv::create());
+        $this->table->appendAction(\Tk\Table\Action\Delete::create());
 
         $this->table->setList($this->getList());
 
