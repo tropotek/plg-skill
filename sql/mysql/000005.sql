2cb61cce97e7d132ada531d3b566f284795783c4
mifsudm <mifsudm@unimelb.edu.au>
2019-07-24 08:24:48 +1000

    ~Auto: Commit

--- sql/mysql/000005.sql
+++ sql/mysql/000005.sql
@@ -6,26 +6,6 @@
 -- Fix the issue with new domains and category ids not being copied correctly when creating a new subject.
 
 
-
-# SELECT *
-# FROM skill_item a LEFT JOIN
-#      skill_domain b ON (a.domain_id = b.id) LEFT JOIN
-#      skill_domain c ON (b.uid = c.uid AND c.collection_id = a.collection_id) LEFT JOIN
-#      skill_category d ON (a.category_id = d.id) LEFT JOIN
-#      skill_category e ON (e.uid = e.uid AND e.collection_id = a.collection_id)
-# WHERE a.collection_id != b.collection_id
-# ;
-
--- Fix all category ids
-UPDATE skill_item a LEFT JOIN
-    skill_domain b ON (a.domain_id = b.id) LEFT JOIN
-    skill_category d ON (a.category_id = d.id) LEFT JOIN
-    skill_category e ON (b.label = e.label AND e.collection_id = a.collection_id)
-SET a.category_id = e.id
-WHERE d.label != '' AND b.label != d.label
-;
-
-
 UPDATE skill_item a LEFT JOIN
     skill_domain b ON (a.domain_id = b.id) LEFT JOIN
     skill_domain c ON (b.uid = c.uid AND c.collection_id = a.collection_id) LEFT JOIN
