f069a720b65d8e7839f8d98f3afc90f94ca860d1
mifsudm <mifsudm@unimelb.edu.au>
2019-12-12 07:48:03 +1100

    ~Auto: Commit

--- sql/mysql/000006.sql
+++ sql/mysql/000006.sql
@@ -6,7 +6,6 @@
 
 
 alter table skill_collection change profile_id course_id int unsigned default 0 not null;
-drop index profile_id on skill_collection;
 create index course_id on skill_collection (course_id);
 drop index skill_collection_subject_id_index on skill_collection;
 create index subject_id on skill_collection (subject_id);
