0a07a00e0a8e201eb6f43120b32aa8477ce769ed
mifsudm <mifsudm@unimelb.edu.au>
2019-12-19 12:45:24 +1100

    ~Auto: Commit

--- Skill/Db/Scale.php
+++ Skill/Db/Scale.php
@@ -2,6 +2,8 @@
 namespace Skill\Db;
 
 
+use Bs\Db\Traits\TimestampTrait;
+
 /**
  * @author Michael Mifsud <info@tropotek.com>
  * @see http://www.tropotek.com/
@@ -9,6 +11,8 @@ namespace Skill\Db;
  */
 class Scale extends \Tk\Db\Map\Model
 {
+
+    use TimestampTrait;
     
     /**
      * @var int
@@ -59,7 +63,7 @@ class Scale extends \Tk\Db\Map\Model
     /**
      * @var Collection
      */
-    private $collection = null;
+    private $_collection = null;
 
 
 
@@ -68,8 +72,7 @@ class Scale extends \Tk\Db\Map\Model
      */
     public function __construct()
     {
-        $this->modified = \Tk\Date::create();
-        $this->created = \Tk\Date::create();
+        $this->_TimestampTrait();
     }
 
     /**
@@ -79,7 +82,7 @@ class Scale extends \Tk\Db\Map\Model
     {
         parent::save();
         try {
-            self::recalculateValues($this->collectionId);
+            self::recalculateValues($this->getCollectionId());
         } catch (\Exception $e) { \Tk\Log::error($e->getMessage()); }
     }
 
@@ -88,10 +91,10 @@ class Scale extends \Tk\Db\Map\Model
      */
     public function getCollection()
     {
-        if (!$this->collection) {
-            $this->collection = CollectionMap::create()->find($this->collectionId);
+        if (!$this->_collection) {
+            $this->_collection = CollectionMap::create()->find($this->getCollectionId());
         }
-        return $this->collection;
+        return $this->_collection;
     }
 
     /**
@@ -104,7 +107,7 @@ class Scale extends \Tk\Db\Map\Model
         $list = ScaleMap::create()->findFiltered(array('collectionId' => $collectionId));
         /** @var Scale $scale */
         foreach ($list as $i => $scale) {
-            $scale->value = $i;
+            $scale->setValue($i);
 //            if ($i == 0) {
 //                $scale->value = 0;
 //            } else {
@@ -124,7 +127,7 @@ class Scale extends \Tk\Db\Map\Model
      */
     public function getRatioValue()
     {
-        $list = ScaleMap::create()->findFiltered(array('collectionId' => $this->collectionId), \Tk\Db\Tool::create('order_by'));
+        $list = ScaleMap::create()->findFiltered(array('collectionId' => $this->getCollectionId()), \Tk\Db\Tool::create('order_by'));
         $cnt = count($list)-1;
         $pos = 0;
         $val = 0;
@@ -142,16 +145,96 @@ class Scale extends \Tk\Db\Map\Model
     }
 
     /**
+     * @return string
+     */
+    public function getUid(): string
+    {
+        return $this->uid;
+    }
+
+    /**
+     * @param string $uid
+     */
+    public function setUid(string $uid): void
+    {
+        $this->uid = $uid;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCollectionId(): int
+    {
+        return $this->collectionId;
+    }
+
+    /**
+     * @param int $collectionId
+     */
+    public function setCollectionId(int $collectionId): void
+    {
+        $this->collectionId = $collectionId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return float
+     */
+    public function getValue(): float
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param float $value
+     */
+    public function setValue(float $value): void
+    {
+        $this->value = $value;
+    }
+
+    /**
      *
      */
     public function validate()
     {
         $errors = array();
 
-        if ((int)$this->collectionId <= 0) {
+        if ($this->getCollectionId() <= 0) {
             $errors['collectionId'] = 'Invalid Collection ID';
         }
-        if (!$this->name) {
+        if (!$this->getName()) {
             $errors['name'] = 'Please enter a valid name';
         }
         
